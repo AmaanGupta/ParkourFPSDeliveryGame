cf671cd5653e493dff5a7673cd9ef9262b74fcfd
ItzAman53 <06amaangupta@gmail.com>
2026-03-26 15:29:07 +0530

    PICKUP COMPLETED

--- Assets/Scripts/PickingScript.cs
+++ Assets/Scripts/PickingScript.cs
@@ -10,8 +10,17 @@ public class PickingScript : MonoBehaviour
     [SerializeField] private GameObject fpsCamera;
     [SerializeField] private LayerMask pickableLayer;
     [SerializeField] private GameObject holdPosition;
+    private Collider objectGrabbed;
+
+    private bool isGrabbed= false;
+    private bool didHitNotLocal=false;
     private float pickupDistance=10f;
-    
+
+    void Update()
+    {
+        if(isGrabbed) Grab();
+    }
+
 
     void OnEnable()
     {
@@ -27,30 +36,49 @@ public class PickingScript : MonoBehaviour
 
     void StartInteract(InputAction.CallbackContext obj)
     {
-        Grab();
+        Debug.Log("Grabbing");
+        bool didHit=Physics.Raycast(fpsCamera.transform.position,fpsCamera.transform.forward,out RaycastHit Hit,pickupDistance,pickableLayer);
+        Debug.Log(didHit);
+        didHitNotLocal=didHit;
+        if (didHit)
+        {
+            objectGrabbed=Hit.collider;
+            Hit.collider.gameObject.GetComponent<Rigidbody>().isKinematic=true;
+            Hit.collider.transform.position=holdPosition.transform.position;
+            Hit.collider.transform.eulerAngles=holdPosition.transform.eulerAngles;
+
+        }
+        isGrabbed=true;
+
     }
     void StopInteract(InputAction.CallbackContext obj)
     {
         Leave();
     }
 
+    
+
     void Grab()
     {
-        Debug.Log("Grabbing");
-        bool didHit=Physics.Raycast(fpsCamera.transform.position,fpsCamera.transform.forward,out RaycastHit Hit,pickupDistance,pickableLayer);
-        Debug.Log(didHit);
-        if (didHit)
+        if (didHitNotLocal)
         {
-            Hit.collider.gameObject.GetComponent<Rigidbody>().isKinematic=true;
-            Hit.collider.transform.position=holdPosition.transform.position;
-
+            objectGrabbed.gameObject.GetComponent<Rigidbody>().isKinematic=true;
+            objectGrabbed.transform.position=holdPosition.transform.position;
+            objectGrabbed.transform.eulerAngles=holdPosition.transform.eulerAngles;
         }
+        
         
         
     }
     void Leave()
     {
+        if (didHitNotLocal)
+        {
+            isGrabbed=false;
+            Debug.Log("leaving");
+            objectGrabbed.gameObject.GetComponent<Rigidbody>().isKinematic=false;
+        }
         
-        Debug.Log("leaving");
+
     }
 }
